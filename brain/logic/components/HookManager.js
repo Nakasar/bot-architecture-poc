b828240fabaa88f3322415338be14f48312658c0
Nakasar <kevin.thizy@outlook.fr>
2018-04-25 11:00:37 +0200

    Documented HookManager.

--- brain/logic/components/HookManager.js
+++ brain/logic/components/HookManager.js
@@ -6,18 +6,39 @@ class HookManager {
     this.io = null;
   }
 
+  /**
+   * Attach the brain socket io server to the HookManager.
+   * @param {socket.io server} io 
+   */
   attachIo(io) {
     this.io = io;
   }
 
+  /**
+   * Create a new hook for a skill.
+   * @param {String} skill 
+   * @return {Promise} Promise to the created hook : { _id, skill }
+   */
   create(skill) {
    return this.hookController.create_hook(skill);
   }
 
+  /**
+   * Confirm a hook.
+   * @param {String} hookId 
+   * @param {String} connectorId
+   * @return {Promise} Promise that resolves if the hook is validated, false otherwise.
+   */
   finalize(hookId, connectorId) {
    return this.hookController.add_connector(hookId, connectorId);
   }
 
+  /**
+   * Execute a hook to contact the connector.
+   * @param {String} hookId 
+   * @param {Object} message - Valid message object to send to the connector.
+   * @return {Promise} Promise that resolves if the message was sent, false otherwise.
+   */
   execute(hookId, message) {
     return new Promise((resolve, reject) => {
       this.get(hookId).then((hook) => {
@@ -42,10 +63,20 @@ class HookManager {
     });
   }
 
+  /**
+   * Remove a hook.
+   * @param {String} hookId
+   * @return {Promise} Promise that resolves if success, false otherwise.
+   */
   remove(hookId) {
    return this.hookController.delete_hook(hookId);
   }
 
+  /**
+   * Get a hook.
+   * @param {String} hookId
+   * @return {Promise} Promise to the hook : { _id, skill, connector }
+   */
   get(hookId) {
    return this.hookController.get_hook(hookId);
   }
--- brain/logic/components/StorageManager.js
+++ brain/logic/components/StorageManager.js
@@ -5,14 +5,28 @@ exports.StorageManager = class StorageManager {
     this.storageController = require("./../../database/controllers/storageController");
   }
 
+  /**
+   * Get an item from the storage.
+   * @param {String} skill 
+   * @param {String} key 
+   */
   getItem (skill, key) {
     return this.storageController.get_storage(skill, key);
   }
 
+  /**
+   * Store a new item in the storage.
+   * @param {String} skill
+   * @param {String} key
+   * @param {Object} value
+   */
   storeItem(skill, key, value) {
     return this.storageController.create_storage(skill, key, value)
   }
 
+  /**
+   * Clear the storage.
+   */
   clear() {
     return this.storageController.clear_storage();
   }
